179bb31ef11cbadbe06db334283efb39bf17f717
Elliott Fouts <efouts@qualtrax.com>
2015-03-17 17:15:00 -0400

    Add batch fill for filling two like directory trees

---
+++ bin/whisper-batch-fill.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import signal
+import optparse
+import shutil
+
+whisper_fill = __import__('whisper-fill')
+
+try:
+  import whisper
+except ImportError:
+  raise SystemExit('[ERROR] Please make sure whisper is installed properly')
+
+def main(argv):
+  # Ignore SIGPIPE
+  signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
+  option_parser = optparse.OptionParser(
+    usage='''%prog [options] from_path to_path''')
+
+  (options, args) = option_parser.parse_args()
+
+  if len(args) < 2:
+    option_parser.print_help()
+    sys.exit(1)
+
+  src = os.path.abspath(args[0])
+  dest = os.path.abspath(args[1])
+
+  srcs = { os.path.relpath(dirpath, src): filenames for (dirpath, dirnames, filenames) in os.walk(src) }
+  dests = { os.path.relpath(dirpath, dest): filenames for (dirpath, dirnames, filenames) in os.walk(dest) }
+
+  for rel in srcs.keys():
+    if dests.has_key(rel):
+      for f in srcs[rel]:
+        s = os.path.join(src, rel, f)
+        d = os.path.join(dest, rel, f)
+
+        if os.path.exists(d): 
+          whisper_fill.main([s, d])
+          print 'fill {0} {1}'.format(s, d)
+        else:
+          shutil.copy(s, os.path.join(d, rel))
+          print 'copy {0} {1}'.format(s, os.path.join(d,rel))
+    else:
+      s = os.path.join(src, rel)
+      d = os.path.join(dest, rel)
+      shutil.copytree(s, d)
+      print 'copytree {0} {1}'.format(s, d)
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
